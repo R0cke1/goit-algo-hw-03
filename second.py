6482e76789208f9fb2bd876f0296c9f24fb189f6
R0cke1 <voskoboinyk.mykhailo@gmail.com>
2024-03-09 15:47:18 +0100

    second

---
+++ second.py
@@ -0,0 +1,23 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+   
+        if not (1 <= min <= max <= 1000) or not (1 <= quantity <= max - min + 1):
+            return [] 
+        numbers = random.sample(range(min, max + 1), quantity)
+        return sorted(numbers)
+   
+
+while True:
+   
+    try:
+        print(get_numbers_ticket(int(input('min:')),int(input('max:')), int(input('quantity:'))))
+        break
+    except ValueError:
+        print ('Вы ввели некорректные данные')
+   
+
+
+
+    
+
