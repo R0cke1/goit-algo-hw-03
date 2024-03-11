e7f782be3d5a723e07df6370ed42878290b606f3
R0cke1 <voskoboinyk.mykhailo@gmail.com>
2024-03-11 09:21:58 +0100

    correction

--- fourth.py
+++ fourth.py
@@ -1,9 +1,22 @@
 from datetime import datetime, timedelta
 
 users = [
-    {"name": "Михайло", "birthday": "2010.03.16"},
-    {"name": "Марія", "birthday": "1996.09.08"},
-    {"name": "Володимир", "birthday": "1987.03.12"}
+    # {"name": "Михайло", "birthday": "2010.03.16"},
+    # {"name": "Марія", "birthday": "1996.09.08"},
+    # {"name": "Володимир", "birthday": "1987.03.12"}
+{'name': 'Liam Smith', 'birthday': '1995.03.18'},
+{'name': 'Mohel Smith', 'birthday': '1995.03.19'},
+{'name': 'John Dark', 'birthday': '1985.03.07'},
+{'name': 'Mary Dark', 'birthday': '1985.03.08'},
+{'name': 'Derek Dark', 'birthday': '1985.03.09'},
+{'name': 'Jane Smith', 'birthday': '1990.03.10'},
+{'name': 'Nick Darsel', 'birthday': '1984.03.11'},
+{'name': 'Liam Smith', 'birthday': '1995.03.12'},
+{'name': 'Mohel Smith', 'birthday': '1995.03.13'},
+{'name': 'John Dark', 'birthday': '1985.03.14'},
+{'name': 'Mary Dark', 'birthday': '1985.03.15'},
+{'name': 'Derek Dark', 'birthday': '1985.03.16'},
+{'name': 'Jane Smith', 'birthday': '1990.03.17'}
 ]
 
 
@@ -24,7 +37,8 @@ def get_upcoming_birthdays(users):
             
             if congratulation_date.weekday() >= 5:    # Якщо день народження випадає на суботу або неділю
                 
-                days_until_monday = (7 - congratulation_date.weekday()) + 1    # Знаходимо наступний понеділок
+                
+                days_until_monday = (7 - congratulation_date.weekday())                
                 congratulation_date += timedelta(days=days_until_monday)
 
             upcoming_birthdays.append({                                                # Додаємо дані про майбутній день народження
